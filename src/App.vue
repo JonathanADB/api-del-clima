851c1c0ad84cb8b0b7bcee9eb90f3cda130d3012
Jonathan <jhonnysoy1@gmail.com>
2024-04-11 19:46:39 +0200

    me pongo con el css

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,58 @@
       <h1 class="mb-5">Weather App</h1>
       <div class="d-flex justify-content-center h-100">
         <div class="searchbar w-50 mx-2">
-          <input type="text" class="input form-control" placeholder="Busca tu ciudad"> 
+          <input type="text" class="input form-control" v-model="city" placeholder="Busca tu ciudad"> 
         </div>
         <button class="btn-search btn btn-primary">Buscar <i class="fas fa-search"></i></button>
       </div>
 
     </div>
+    <br>
+    <Weather></Weather>
   </div>
 </template>
 
 <script>
 
+import Weather from './components/Weather.vue';
 
-export default {
+export default (await import('vue')).defineComponent(  {
   name: 'App',
   components: {
-   
+    Weather
+},
+data(){
+  return{
+    city: '',
+    showWeather: false,
+  }
+},
+methods: {
+  async searchWeather () {
+    this.showWeather = false;
+    await this.$nextTick();
+    this.showWeather = true;
   }
 }
+})
 </script>
 
 <style>
+
+body{
+  background-color: #121212 !important;
+}
+
+.header{
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin-top: 5rem; 
+}
+ .btn-search{
+  background-image: linear-gradient(to right, cyan, magenta);
+ }
 
 </style>
