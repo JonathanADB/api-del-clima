f151e6e716480787c3c352d018cf5c9df923df8b
JonathanADB <jhonnysoy1@gmail.com>
2024-04-11 14:17:20 +0200

    vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <div class="header container h-100 p-5">
+      <h1 class="mb-5">Weather App</h1>
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar w-50 mx-2">
+          <input type="text" class="input form-control" placeholder="Busca tu ciudad"> 
+        </div>
+        <button class="btn-search btn btn-primary">Buscar <i class="fas fa-search"></i></button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
